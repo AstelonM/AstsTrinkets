09986f86a8285cc1e2c0561116104e69acf04a4b
Mihai <banicamihai96@gmail.com>
2022-11-15 17:57:01 +0200

    Small visual update to the True Invisibility Tunic

--- src/main/java/com/astelon/aststrinkets/trinkets/TrueInvisibilityTunic.java
+++ src/main/java/com/astelon/aststrinkets/trinkets/TrueInvisibilityTunic.java
@@ -4,6 +4,7 @@ import com.astelon.aststrinkets.AstsTrinkets;
 import com.astelon.aststrinkets.Power;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextColor;
 import org.bukkit.Color;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
@@ -24,13 +25,13 @@ public class TrueInvisibilityTunic extends Trinket {
         ItemStack itemStack = new ItemStack(Material.LEATHER_CHESTPLATE);
         LeatherArmorMeta meta = (LeatherArmorMeta) itemStack.getItemMeta();
         meta.setUnbreakable(true);
-        meta.setColor(Color.fromRGB(38, 50, 101));
-        meta.displayName(Component.text("The True Tunic of Invisibility", NamedTextColor.DARK_BLUE));
-        meta.lore(List.of(Component.text("Much stronger and more durable than the"),
-                Component.text().append(Component.text("Tunic of Invisibility", NamedTextColor.GRAY))
-                        .append(Component.text(", this one makes")).build(),
-                Component.text("the wearer invisible to everyone,"),
-                Component.text("no matter how good their eyesight is.")));
+        meta.setColor(Color.fromRGB(44, 48, 101));
+        meta.displayName(Component.text("The True Tunic of Invisibility", TextColor.fromHexString("#2C3065")));
+        meta.lore(List.of(Component.text("Far more powerful than the Tunic"),
+                Component.text("of Invisibility, this unbreakable"),
+                Component.text("artifact will make the wearer"),
+                Component.text("invisible to everyone, no matter"),
+                Component.text("how good their eyesight is.")));
         itemStack.setItemMeta(meta);
         return itemStack;
     }
