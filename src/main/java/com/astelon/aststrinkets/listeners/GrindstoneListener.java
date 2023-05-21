d496e4aba7a4899683b2f91fb802b40b20c260b9
Banica Mihai <banicamihai96@gmail.com>
2023-05-21 17:52:41 +0300

    Excluded the Souleater from the grindstone blacklist

--- src/main/java/com/astelon/aststrinkets/listeners/GrindstoneListener.java
+++ src/main/java/com/astelon/aststrinkets/listeners/GrindstoneListener.java
@@ -1,6 +1,7 @@
 package com.astelon.aststrinkets.listeners;
 
 import com.astelon.aststrinkets.managers.TrinketManager;
+import com.astelon.aststrinkets.trinkets.Souleater;
 import com.destroystokyo.paper.event.inventory.PrepareResultEvent;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -10,16 +11,18 @@ import org.bukkit.inventory.ItemStack;
 public class GrindstoneListener implements Listener {
 
     private final TrinketManager trinketManager;
+    private final Souleater souleater;
 
     public GrindstoneListener(TrinketManager trinketManager) {
         this.trinketManager = trinketManager;
+        souleater = trinketManager.getSouleater();
     }
 
     @EventHandler
     public void onPrepareResult(PrepareResultEvent event) {
         if (event.getInventory() instanceof GrindstoneInventory inventory) {
             ItemStack itemStack = inventory.getResult();
-            if (itemStack != null && trinketManager.isTrinket(itemStack)) {
+            if (itemStack != null && trinketManager.isTrinket(itemStack) && !souleater.isTrinket(itemStack)) {
                 event.setResult(new ItemStack(itemStack.getType()));
             }
         }
