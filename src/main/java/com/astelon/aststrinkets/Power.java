da083dcb595bbcbee285501417b8fef382148831
Mihai Bănică <banicamihai96@gmail.com>
2025-02-12 21:57:37 +0200

    Fixed typo in power name

--- src/main/java/com/astelon/aststrinkets/Power.java
+++ src/main/java/com/astelon/aststrinkets/Power.java
@@ -58,7 +58,7 @@ public enum Power {
     SELECT_RANDOM_NUMBER_WHEN_THROWN("selectRandomNumberWhenThrown"),
     RANDOM_TELEPORTATION("randomTeleportation"),
     REUSABLE_RANDOM_TELEPORTATION("reusableRandomTeleportation"),
-    NETHER_ZOMBIFICATION_CURE("netherZombiFicationCure"),
+    NETHER_ZOMBIFICATION_CURE("netherZombificationCure"),
     MAKE_ITEM_FRAMES_INVISIBLE("makeItemFramesInvisible"),
     VIRUS("virus"),
     OXIDIZE_COPPER("oxidizeCopper"),
